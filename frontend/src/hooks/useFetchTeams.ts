655263f8e7cfa037edcd20089d87dbc6da443c75
Seb <linoshizu@gmail.com>
2025-04-05 23:56:38 -0300

    Update useFetchTeams
    This will update the cases strings by making them plular to make sense

--- frontend/src/hooks/useFetchTeams.ts
+++ frontend/src/hooks/useFetchTeams.ts
@@ -1,23 +1,22 @@
 import { useReducer, useEffect } from "react";
 import { teamsReducer, initialTeamsState } from "../reducers/teamsReducer";
 import { getTeams } from "../api/teams";
-import { ITeamsState } from "../interfaces/index";
 
 function useFetchTeams() {
-    const [state, dispatch] = useReducer(teamsReducer, initialTeamsState as ITeamsState);
+    const [state, dispatch] = useReducer(teamsReducer, initialTeamsState);
 
     useEffect(() => {
         const fetchTeams = async () => {
-            dispatch({ type: "FETCH_TEAM_REQUEST", payload: null });
+            dispatch({ type: "FETCH_TEAMS_REQUEST", payload: null });
 
             try {
                 const teamsData = await getTeams();
-                dispatch({ type: "FETCH_TEAM_SUCCESS", payload: teamsData });
+                dispatch({ type: "FETCH_TEAMS_SUCCESS", payload: teamsData });
             } catch (error) {
                 if (error instanceof Error) {
-                    dispatch({ type: "FETCH_TEAM_FAILURE", payload: { message: error.message } });
+                    dispatch({ type: "FETCH_TEAMS_FAILURE", payload: { message: error.message } });
                 };
-            }
+            };
         };
 
         fetchTeams();
